70b996ed3b201a2ef435afe4625554c0565bf017
Venkat reddy <venkatreddynandyala8@gmail.com>
2021-12-01 17:42:16 +0530

    Implemented Routing

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,14 +1,17 @@
 import "./footer.css";
+import { Link } from 'react-router-dom';
 import {BsPerson,BsFillCollectionPlayFill,BsHouseDoor,BsHeart,BsSearch } from "react-icons/bs";
+import { paths } from "../../routes/paths";
+
 function Footer() {
 	return (
 		<div className="footer">
 			<div className="footer-icons">
-				<BsHouseDoor/>
-				<BsSearch />
-				<BsFillCollectionPlayFill/>
-				<BsHeart/>
-				<BsPerson/>
+				<Link to={paths.home}><BsHouseDoor /></Link>
+				<Link to={paths.search}><BsSearch /></Link>
+				<Link to={paths.reels}><BsFillCollectionPlayFill /></Link>
+				<Link to={paths.notifications}><BsHeart /></Link>
+				<Link to={paths.profile}><BsPerson /></Link>
 			</div>
 		</div>
 	);
